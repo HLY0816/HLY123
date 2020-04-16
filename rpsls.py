65de31d1192552283d520dc64670259730d46ef4
HLY0816 <63770938+HLY0816@users.noreply.github.com>
2020-04-16 22:35:59 +0800

    Add files via upload

--- rpsls.py
+++ rpsls.py
@@ -3,7 +3,7 @@
 目标：新编一程序，完成RPSLS游戏，即石头剪刀布蜥蜴史波克
 作者：黄龙艳
 """
-import random#调用random函数
+import random
 
 
 
@@ -13,6 +13,9 @@ import random#调用random函数
 # 3 - 蜥蜴
 # 4 - 剪刀
 def name_to_number(name):
+   """
+   将游戏对象对应到不同的整数
+   """
    if(name == '石头'): 
 	   return 0
    elif(name == '史波克'): 
@@ -26,6 +29,9 @@ def name_to_number(name):
    else:
        print("Errer:No Correct Name")
 def number_to_name(number):
+   """
+   将整数(0,1,2,3 or 4)对应到不同的游戏对象
+   """
    if(number == 0): 
 	   return '石头'
    elif(number == 1):
@@ -38,6 +44,9 @@ def number_to_name(number):
 	   return '剪刀'
    else:print("Errer:No Correct Name")
 def rpsls(player_choice):
+   """
+   用户玩家任意给出一个选择，根据RPSLS游戏规则在屏幕输出对应的结果
+   """
    print("------------------")
    print("player_choose"+player_choice)
    player_number=name_to_number(player_choice)#将用户的选择用name_to_number函数表示出来
